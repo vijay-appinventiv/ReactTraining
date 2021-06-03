22a34c43b04f0ddbbd680ad88e95da6ea60e5a04
Vijay Sharma <vijay.sharma@appinventiv.com>
2021-06-03 10:00:18 +0530

    added reusable component inside class component.

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './index.css';
 class Home extends Component {
+
+    renderBox(heading, buttonTitle, extraClass = "", color = "") {
+        return (
+            <div className={"itemContainer"}>
+                <h3 className={extraClass}>{heading}</h3>
+                <button style={{ backgroundColor: color }}>{buttonTitle}</button>
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className={"homeContainer"}>
-                <h1 className={"h1tag"}>{"Hello from Home"}</h1>
-                <h2 id={"h2tag"}>{"Hello from Home"}</h2>
-                <h3>{"Hello from Home"}</h3>
+                <h1>{"Home Page"}</h1>
+                {this.renderBox("First Container", "Click Me First", "redClrTxt")}
+                {this.renderBox("Second Container", "Click Me Second", "", "blue")}
+                {this.renderBox("Third Container", "Click Me Third")}
             </div>
         );
     }
